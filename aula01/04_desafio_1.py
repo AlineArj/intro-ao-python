4b283594541812ff665351341dc8354960a32fba
AlineArj <alinearj@usp.br>
2022-04-20 19:43:43 -0300

    exercícios aula 1

--- aula01/04_desafio_1.py
+++ aula01/04_desafio_1.py
@@ -1,3 +1,8 @@
 # 1. Peça ao seu usuário para inserir 3 dados: nome, idade e cidade.
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+cidade = str(input('Digite sua cidade: '))
 
 # 2. Imprima no console uma única mensagem desejando boas vindas ao usuário.
+print(f'Seja bem-vindo(a) {nome}!')
+print(f'A idade do usuário é: {idade}\nA cidade do usuário é: {cidade}')
--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,8 +1,12 @@
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
 
+cidade = str(input('Digite a sua cidade: '))
+estado = str(input('Digite o seu estado: '))
 # 2. Imprima uma mensagem diga a cidade em que o usuário mora. 
 #    O nome da cidade deve estar todo em letras maiúsculas.
+print(f'A cidade do usuário é {cidade.upper()}')
 
 # 3. Imprima uma mensagem diga o estado em que o usuário mora. 
 #    O nome do estado deve estar todo em letras maiúsculas.
 #    Utilize uma forma de formatação de string diferente da que foi usada no item 2.
+print('O estado do usuário é ' + str(estado.upper()))
