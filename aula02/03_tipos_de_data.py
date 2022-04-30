fbf46252a541811e76c3b169d510a734d053dc40
AlineArj <alinearj@usp.br>
2022-04-29 21:26:29 -0300

    add comentários de estudo

--- aula02/03_tipos_de_data.py
+++ aula02/03_tipos_de_data.py
@@ -2,6 +2,7 @@
 # falar de módulos nas próximas aulas
 from datetime import datetime, date, time, timedelta
 
+# --- DATA E HORA (datetime) ---
 # O tipo datetime oferece o método now para retornar o exato momento da chamada da função
 agora = datetime.now()
 print('Agora: ' + str(agora))
@@ -10,7 +11,12 @@ print('Agora: ' + str(agora))
 # no Brasil. Para imprimir a data da forma que conhecemos, precisamos
 # utilizar a função strftime para indicar para o interpretador qual o formato
 # que esperamos
-print('Agora: ' + agora.strftime('%d/%m/%Y %H:%M:%S'))
+print('Agora formato BR: ' + agora.strftime('%d/%m/%Y %H:%M:%S'))
+
+# DATA: dia -> %d | mês -> %m | ano -> %Y
+# HORÁRIO: hora -> %H | minuto -> %M | segundo -> %S
+
+print()
 
 # O "objeto" com o tipo datetime permite o acesso direto a cada
 # um dos itens dentro do tempo. 
@@ -21,12 +27,17 @@ print('Hora: ' + str(agora.hour))
 print('Minuto: ' + str(agora.minute))
 print('Segundo: ' + str(agora.second))
 
+print()
+
 # Também podemos inicializar um objeto de datetime para o dia e a hora que
 # queremos. Note que o VSCode ajuda você a preencher os parâmetros da função
 # que inicializa um datetime
 aniversario_da_dori = datetime(2022, 2, 22, 8, 30, 55)
 print('Aniversário da Dori: ' + aniversario_da_dori.strftime('%d/%m/%Y %H:%M:%S'))
 
+print()
+
+# --- APENAS HORA (.time()) ---
 # Assim como existe o datetime, também temos tipos separados
 # para a hora e o dia. Podemos inicializar um objeto do tipo tempo
 # a partir de um objeto do tipo datetime ou especificar os valores
@@ -41,15 +52,20 @@ print('Tipo da "hora": ', str(type(hora)))
 hora_de_dormir = time(22,30,00)
 print('Hora: ' + hora_de_dormir.strftime('%H:%M:%S'))
 
+print()
+
+# --- APENAS DATA (.date()) ---
 # De forma semelhante, existe o tipo dia. Ele também pode ser inicializado a partir de
 # um objeto datetime ou utilizando os valores especificamente
 dia_do_aniversario_da_dori = aniversario_da_dori.date()
-# dia_do_aniversario_da_dori = date(2020,2,22) -> poderia inicializar o objeto assim também
+#dia_do_aniversario_da_dori = date(2020,2,22) -> poderia inicializar o objeto assim também
 print('Dia do niver da Dodó: ' + dia_do_aniversario_da_dori.strftime('%d/%m/%y'))
 print('Tipo da "dia_do_aniversario_da_dori": ', str(type(dia_do_aniversario_da_dori)))
 
+print()
+
 # Podemos fazer contas com datas utilizando o tipo timedelta
-hoje = datetime.now().date()
+hoje = datetime.today()
 print('Hoje é: ' + hoje.strftime('%d/%m/%y'))
 
 #You can use timedelta to add or remove days, or weeks to a date
@@ -57,9 +73,13 @@ um_dia = timedelta(days=1)
 ontem = hoje - um_dia
 print('Ontem foi: ' + ontem.strftime('%d/%m/%y'))
 
+print()
+
 uma_semana = timedelta(weeks=1)
 semana_passada = hoje - uma_semana
 print('Há uma semana era: ' + semana_passada.strftime('%d/%m/%y'))
+
+print()
 
 # Também podemos converter strings para objetos datetime com a função
 # 'strptime'
