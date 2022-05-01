f0399697ce25a526f7601c18b81628f2b967813a
AlineArj <alinearj@usp.br>
2022-05-01 19:24:00 -0300

    add colorama ao desafio 6

--- aula03/06_desafio_6.py
+++ aula03/06_desafio_6.py
@@ -2,12 +2,16 @@
 # Você quer criar um sistema no qual suas alunas podem ver suas notas digitando o nome.
 # As listas de alunas e notas se encontra abaixo. As notas podem variar de 0 a 100.
 # Nessas listas, as notas estão na mesma ordem dos nomes, ou seja, a aluna no índice 4 tirou a nota no índice 4.
+import colorama
+
+colorama.init()
+
 alunas = ['Adriana', 'Bárbara', 'Franciele', 'Laís', 'Maria', 'Nayara', 'Patrícia', 'Renata', 'Sarah', 'Thainá']
 notas = [78, 57, 80, 98, 54, 89, 90, 100, 71, 85]
 
 # Comece o programa perguntando o nome da aluna.
 nome_pesquisa = input('Nome: ')
-print('-' * 20)
+print(colorama.Fore.CYAN + '-' * 20 + colorama.Style.RESET_ALL)
 print()
 
 # Procure o nome digitado na lista de nomes e imprima uma mensagem com a nota que ela tirou.
@@ -20,13 +24,13 @@ if nome_pesquisa in alunas:
             print(f'Aluna: {aluna}')
 
             if notas[indice] < 60:
-                print(f'Nota: \033[0;31m{notas[indice]}\033[m')
+                print(f'Nota: ' + colorama.Fore.RED + str(notas[indice]))
             elif notas[indice] >= 90:
-                print(f'Nota: \033[0;32m{notas[indice]}\033[m')
+                print(f'Nota: ' + colorama.Fore.GREEN + str(notas[indice]))
             else:
                 print(f'Nota: {notas[indice]}')
 
 else:
-    print('\033[0;30;41m  ERRO: Nome não encontrado  \033[m')
+    print(colorama.Back.RED + '  ERRO: Nome não encontrado  ' + colorama.Style.RESET_ALL)
 
 print()
