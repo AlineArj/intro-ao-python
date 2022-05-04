2d033bd3754b34f819fcb4e0ad6f4d1f066d05b5
AlineArj <alinearj@usp.br>
2022-05-04 20:27:17 -0300

    add desafio 11

--- aula06/04_desafio_11.py
+++ aula06/04_desafio_11.py
@@ -2,5 +2,10 @@
 # e escreva um outro arquivo de saída que imprima o texto ao contrário.
 # Exemplo de entrada: Oi mulheres maravilhosas do curso de Python do ConstruDelas!
 # Exemplo de saída: !saleDurtsnoC od nohtyP ed\nosruc od sasohlivaram serehlum iO
+caminho01 = 'entrada_desafio_11.txt'
+with open(caminho01, 'r') as arquivo01:
+    text = arquivo01.read()
 
-arquivo = 'entrada_desafio_11.txt'
+caminho02 = 'saida_desafio_11.txt'
+with open(caminho02, 'w') as arquivo02:
+    arquivo02.write(text[::-1])
